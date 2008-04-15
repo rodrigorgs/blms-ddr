36d0489951d170207356763892cd25bebc59ec29
rodrigorgs <rodrigorgs@a68f0217-5d4a-0410-a9b5-7d53eb537e58>
2008-04-15 14:54:50 +0000

    Implementando criação de ligas. us-leagues.txt: 25 tests Ok.

--- src/script/roda-easy-accept.sh
+++ src/script/roda-easy-accept.sh
@@ -1,1 +1,1 @@
-java -cp ../../bin:../../jars/easyaccept.jar easyaccept.EasyAccept blms.facade.BlmsFacade ../test/easyaccept/us-users.txt
+java -cp ../../bin:../../jars/easyaccept.jar easyaccept.EasyAccept blms.facade.BlmsFacade ../test/easyaccept/us-{users,leagues}.txt
