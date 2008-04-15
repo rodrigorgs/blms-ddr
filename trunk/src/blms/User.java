f394edc800d726b641c7a65a082c27779ffbaad6
rodrigorgs <rodrigorgs@a68f0217-5d4a-0410-a9b5-7d53eb537e58>
2008-04-15 00:19:53 +0000

    Implementando restrições sobre atributos de usuários. Restrições por enquanto só são verificadas na criação de usuários. Passando em 51 testes.

--- trunk/src/blms/User.java
+++ trunk/src/blms/User.java
@@ -1,12 +1,18 @@
 package blms;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import util.StringUtil;
+
 public class User {
 	String firstName, lastName, homePhone, workPhone, cellPhone, email;
 	String picture;
 	
 	public User(String firstName, String lastName, String homePhone,
 			String workPhone, String cellPhone, String email, 
-			String picture) {
+			String picture) throws Exception {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.homePhone = homePhone;
@@ -14,6 +20,33 @@ public class User {
 		this.cellPhone = cellPhone;
 		this.email = email;
 		this.picture = picture;
+		
+		validateAttributes();
+	}
+	
+	private boolean isNullOrEmpty(String s) {
+		return (s == null || s.trim().length() == 0);
+	}
+	
+	private void validateAttributes() throws Exception {
+		String[] missing = missingAttributes();
+		if (missing.length > 0)
+			throw new Exception("Required data: " + Util.join(missing, ", "));
+		if (isNullOrEmpty(homePhone)
+				&& isNullOrEmpty(workPhone)
+				&& isNullOrEmpty(cellPhone))
+			throw new Exception("Need at least one phone");
+	}
+	
+	private String[] missingAttributes() {
+		Collection<String> list = new LinkedList<String>();
+		if (isNullOrEmpty(firstName))
+			list.add("first name");
+		if (isNullOrEmpty(lastName))
+			list.add("last name");
+		if (isNullOrEmpty(email))
+			list.add("email");
+		return list.toArray(new String[] {});
 	}
 
 	public String toString() {
