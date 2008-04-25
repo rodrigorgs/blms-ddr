60426ab11b3124707f5bf68cb5152fde519e2c6a
rodrigorgs <rodrigorgs@a68f0217-5d4a-0410-a9b5-7d53eb537e58>
2008-04-25 18:44:38 +0000

    Incluindo biblioteca JEPLite para parse de expressões numéricas.

---
+++ trunk/src/blms/sandbox/Sandbox.java
@@ -0,0 +1,77 @@
+package blms.sandbox;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+import org.cheffo.jeplite.JEP;
+
+class Inteiro implements Comparable {
+	int x;
+	public Inteiro(int i) {
+		x = i;
+	}
+	public void set(int i) {
+		x = i;
+	}
+	@Override
+	public int compareTo(Object o) {
+		int y = ((Inteiro)o).x;
+		return x - y;
+	}
+	
+	public String toString() {
+		return "" + x;
+	}
+}
+
+
+public class Sandbox {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		treeSet();
+		toArray();
+		date();
+		expression();
+	}
+	
+	private static void expression() throws Exception {
+		JEP jep = new JEP();
+		jep.addVariable("x", 2);
+		jep.parseExpression("x+x");
+		System.out.println(jep.getValue());
+	}
+
+	private static void date() throws Exception {
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+		Date date = format.parse("31-02-2008");
+	}
+
+	private static void toArray() {
+		LinkedList<String> list = new LinkedList<String>();
+		String x = "abc";
+		String y = "def";
+		list.add(x);
+		list.add(y);
+		String[] strs = list.toArray(new String[] {});
+		Arrays.sort(strs);
+		assert strs[0] == x;
+	}
+
+	private static void treeSet() {
+		TreeSet<Inteiro> set = new TreeSet<Inteiro>();
+		Inteiro i1 = new Inteiro(1);
+		Inteiro i2 = new Inteiro(2);
+		set.add(i2);
+		set.add(i1);
+		System.out.println(Arrays.toString(set.toArray()));
+		i1.set(3);
+		System.out.println(Arrays.toString(set.toArray()));
+	}
+
+}
