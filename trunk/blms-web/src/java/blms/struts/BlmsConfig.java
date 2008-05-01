be8785a341de3678ea902e1a4e7b875b26c9e6d0
rodrigorgs <rodrigorgs@a68f0217-5d4a-0410-a9b5-7d53eb537e58>
2008-05-01 19:37:47 +0000

    Fazendo páginas...

---
+++ trunk/blms-web/src/java/blms/struts/BlmsConfig.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package blms.struts;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class BlmsConfig {
+    public static final String DBNAME = "pageDB2";
+}
