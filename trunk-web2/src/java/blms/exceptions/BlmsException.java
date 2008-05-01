7d7532d4977545884efbc164c0bb759a1ef19f39
rodrigorgs <rodrigorgs@a68f0217-5d4a-0410-a9b5-7d53eb537e58>
2008-05-01 13:52:18 +0000

    Removendo código-fonte do blms. Agora o blms é usado através do blms.jar.

--- trunk-web2/src/java/blms/exceptions/BlmsException.java
+++
@@ -1,9 +0,0 @@
-package blms.exceptions;
-
-public class BlmsException extends Exception {
-	private static final long serialVersionUID = 120633416572705794L;
-
-	public BlmsException(String message) {
-		super(message);
-	}
-}
