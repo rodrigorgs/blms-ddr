3db169a209592d5d31126828cb660a0cceba1110
rodrigorgs <rodrigorgs@a68f0217-5d4a-0410-a9b5-7d53eb537e58>
2008-05-01 14:36:33 +0000

    Movendo trunk-web2 para trunk/blms-web.

---
+++ blms-web/README.txt
@@ -0,0 +1,48 @@
+Billiards League Management System
+Web Interface
+(BLMS-web)
+
+== Dependencies ==
+
+You must have Apache Tomcat properly installed and running.
+From now on we'll assume that Tomcat is accessible via http://localhost:8080/
+
+You must have Billiards League Management System (the core application).
+
+== File structure ==
+
+If you have download BLMS and BLMS-WEB, your file structure should be
+something like this:
+
+  blms       # the core BLMS application
+  blms-web   # BLMS-web
+    /jars
+
+== Installing ==
+
+Go to the blms folder (from the core application). Run the command
+
+  ant jar
+
+This command will create a file name blms.jar. Copy it to blms-web/jars.
+
+Run the command
+
+  ant run-deploy
+
+(Please ignore the message BUILD FAILED).
+The web application will be deployed at /blms-web
+
+== Running ==
+
+Open a web browse and enter the following address:
+
+http://localhost:8080/blms-web/
+
+== Uninstalling ==
+
+Run the command
+
+  ant run-undeploy
+
+
---
+++ blms-web/src/java/blms/actions/CreateUserAction.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package blms.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionForward;
+import blms.actionforms.CreateUserForm;
+import blms.facade.BlmsFacade;
+
+/**
+ *
+ * @author drsantos
+ */
+public class CreateUserAction extends org.apache.struts.action.Action {
+    
+    /* forward name="success" path="" */
+    private final static String SUCCESS = "success";
+    private final static String OPERATION = "user creation";
+    
+    /**
+     * This is the action called from the Struts framework.
+     * @param mapping The ActionMapping used to select this instance.
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     * @param response The HTTP Response we are processing.
+     * @throws java.lang.Exception
+     * @return
+     */
+    public ActionForward execute(ActionMapping mapping, ActionForm  form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        CreateUserForm userForm = (CreateUserForm)form;
+        
+        BlmsFacade facade = new BlmsFacade();
+        facade.useDatabase("pageDB2");
+        
+        facade.createUser(userForm.getFirstName(), userForm.getLastName(), 
+                userForm.getHomePhone(), userForm.getWorkPhone(), userForm.getCellPhone(),
+                userForm.getEmail(), "");
+        
+        facade.closeDatabase();
+        request.setAttribute("operation", OPERATION);
+        return mapping.findForward(SUCCESS);
+    }
+}
